6e59a46396d43ba573837bc444dcb0047af76a78
Hubert Thieriot <hubert@energyandcleanair.org>
2021-10-15 19:13:26 +0800

    -update_generation functions -eia series missing

--- R/eia.R
+++ R/eia.R
@@ -1,5 +1,6 @@
 eia.init <- function(){
   readRenviron(".env")
+  readRenviron(".Renviron")
   eia::eia_set_key(Sys.getenv("EIA_KEY"))
 }
 
@@ -28,30 +29,47 @@ eia.update_generation <- function(year){
   dir.create("cache/eia", showWarnings = F, recursive = T)
   f <- file.path("cache/eia/", sprintf("gen_%d.RDS",year))
   
+  
+  series <- list(
+    Coal="EBA.US48-ALL.NG.COL.H",
+    `Fossil Gas`="EBA.US48-ALL.NG.NG.H",
+    Nuclear="EBA.US48-ALL.NG.NUC.H",
+    Oil="EBA.US48-ALL.NG.OIL.H",
+    Other="EBA.US48-ALL.NG.OTH.H",
+    Solar="EBA.US48-ALL.NG.SUN.H",
+    Wind="EBA.US48-ALL.NG.WND.H",
+    Hydro="EBA.US48-ALL.NG.WAT.H"
+  )
+  
+  
   if(file.exists(f)){
     d <- readRDS(f)
     last_update <- max(eia::eia_series_updates(id=series)$updated) %>%
       lubridate::as_datetime()
-    need_update <- (last_update > max(d$date) + lubridate::hours(24)) & (lubridate::year(last_update)==lubridate::year(max(d$date)))  
+    need_update <- (nrow(d)==0) || 
+      ((last_update > max(d$date) + lubridate::hours(24)) & (lubridate::year(last_update)==lubridate::year(max(d$date))))
   }else{
     need_update <- T
   }
   
   if(need_update){
     d <- lapply(names(series), function(source){
-      eia::eia_series(series[[source]],
-                      cache=F,
-                      n=365*24,
-                      start=year,
-                      end=year) %>%
-        select(unit=units, data) %>%
-        tidyr::unnest(data) %>%
-        filter(unit=="megawatthours") %>%
-        mutate(source=source,
-               iso2="US",
-               region="United States"
-        ) %>%
-        select(iso2, region, source, date, output_mw=value)
+      tryCatch({
+        eia::eia_series(series[[source]],
+                        cache=F,
+                        n=365*24,
+                        start=year,
+                        end=year) %>%
+          select(unit=units, data) %>%
+          tidyr::unnest(data) %>%
+          filter(unit=="megawatthours") %>%
+          mutate(source=!!source,
+                 iso2="US",
+                 region="United States",
+                 data_source="eia"
+          ) %>%
+          select(iso2, region, data_source, date, source, output_mw)
+      }, error=function(e){return(tibble())})
     }) %>% do.call(bind_rows, .)
     
     saveRDS(d, f)
--- R/entso.R
+++ R/entso.R
@@ -20,13 +20,103 @@ entso.get_generation <- function(
   
   # Update cache from EIA
   pbapply::pblapply(iso2s, function(iso2){
-    lapply(years, function(year){entso.update_generation(iso2=iso2, year=year)})
+    print(iso2)
+    lapply(years, function(year){
+      print(year)
+      entso.update_generation(iso2=iso2, year=year)
+      })
   })
   
   # Return requested generation
   lapply(iso2s, function(iso2){
     lapply(years, function(year){
       readRDS(sprintf("cache/entso/gen_%s_%d.RDS", iso2, year)) %>%
+        filter(quantity_Measure_Unit.name=="MAW") %>%
+        mutate(iso2=iso2,
+               region=countrycode::countrycode(iso2, "iso2c", "country.name"),
+               data_source="entso") %>%
+        rename(
+          date=dt,
+          output_mw=quantity,
+          codes=MktPSRType
+        ) %>%
+        left_join(
+          entsoeapi::en_generation_codes() %>%
+            select(codes, source=meaning)
+        ) %>%
+        select(iso2, region, data_source, date, source, output_mw)
+    }) %>%
+      do.call(bind_rows, .) %>%
+      filter(date>=as.Date(date_from),
+             date<=as.Date(date_to))
+  }) %>%
+    do.call(bind_rows, .)
+}
+
+
+
+entso.update_generation <- function(iso2, year){
+  
+  dir.create("cache/entso", showWarnings = F, recursive = T)
+  f <- file.path("cache/entso/", sprintf("gen_%s_%d.RDS",iso2,year))
+  
+  eic <- entsoeapi::en_eic() %>%
+    filter(AreaTypeCode=="CTY") %>%
+    filter(MapCode==iso2) %>%
+    pull(AreaCode)
+  
+  
+  get_gen_safe <- function(eic, date_from, date_to){
+    tryCatch({
+      entsoeapi::en_generation_agg_gen_per_type(eic=eic,
+                                              period_start=date_from,
+                                              period_end=date_to,
+                                              security_token=Sys.getenv("ENTSO_TOKEN"))
+    }, error=function(e){return(tibble())})
+  }
+  
+  
+  if(file.exists(f)){
+    d <- readRDS(f)
+    d <- bind_rows(
+      get_gen_safe(eic, max(d$dt), as.POSIXct(paste0(year,"-12-31"), tz="UTC")),
+      d) %>%
+      distinct()
+  }else{
+    d <- get_gen_safe(eic, as.POSIXct(paste0(year,"-01-01"), tz="UTC"), as.POSIXct(paste0(year,"-12-31"), tz="UTC"))
+  }
+
+  saveRDS(d, f)
+  
+}
+
+
+
+
+
+
+
+entso.get_capacity <- function(
+  iso2s=NULL,
+  years=lubridate::year(lubridate::today())){
+  
+  if(is.null(iso2s)){
+    iso2s <-  entsoeapi::en_eic() %>%
+      filter(AreaTypeCode=="CTY") %>%
+      pull(MapCode) %>%
+      unique()
+  }
+  
+  
+  # Update cache from EIA
+  pbapply::pblapply(iso2s, function(iso2){
+    lapply(years, function(year){entso.update_capacity(iso2=iso2, year=year)})
+  })
+  
+  # Return requested generation
+  lapply(iso2s, function(iso2){
+    lapply(years, function(year){
+      readRDS(sprintf("cache/entso/cap_%s_%d.RDS", iso2, year)) %>%
         filter(quantity_Measure_Unit.name=="MAW") %>%
         mutate(iso2=iso2,
                region=countrycode::countrycode(iso2, "iso2c", "country.name")) %>%
@@ -49,10 +139,12 @@ entso.get_generation <- function(
 }
 
 
-entso.update_generation <- function(iso2, year){
+
+
+entso.update_capacity <- function(iso2, year, n_days_refresh=7){
   
   dir.create("cache/entso", showWarnings = F, recursive = T)
-  f <- file.path("cache/entso/", sprintf("gen_%s_%d.RDS",iso2,year))
+  f <- file.path("cache/entso/", sprintf("cap_%s_%d.RDS",iso2,year))
   
   eic <- entsoeapi::en_eic() %>%
     filter(AreaTypeCode=="CTY") %>%
@@ -61,20 +153,21 @@ entso.update_generation <- function(iso2, year){
   
   if(file.exists(f)){
     d <- readRDS(f)
-    d <- bind_rows(
-      entsoeapi::en_generation_agg_gen_per_type(eic=eic,
-                                                period_start=max(d$dt),
-                                                period_end=as.POSIXct(paste0(year,"-12-31"), tz="UTC"),
-                                                security_token=Sys.getenv("ENTSO_TOKEN")),
-      d) %>%
-      distinct()
-  }else{
-    d <- entsoeapi::en_generation_agg_gen_per_type(eic=eic,
-                                                   period_start=as.POSIXct(paste0(year,"-01-01"), tz="UTC"),
-                                                   period_end=as.POSIXct(paste0(year,"-12-31"), tz="UTC"),
-                                                   security_token=Sys.getenv("ENTSO_TOKEN"))
+    # Update only if required (i.e. only if within year and older than n days)
+    need_update <- is.na(d) ||
+      (unique(d$dt_created) < unique(d$end)) &
+      (unique(d$dt_created) < lubridate::today() - n_days_refresh)
   }
-
-  saveRDS(d, f)
   
+  if(!file.exists(f) || need_update){
+    d <- tryCatch({
+      entsoeapi::en_generation_inst_gen_cap_agg(eic=eic,
+                                                year=year,
+                                                security_token=Sys.getenv("ENTSO_TOKEN"))
+    }, error=function(e){
+      warning("Failed getting capacity for ", iso2, ": ", e)
+      return(NA)})
+      
+    saveRDS(d, f)  
+  }
 }

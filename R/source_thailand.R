7f192c099605abc4bca8ed14d8ed0ee83ce182b4
Danny Hartono <danny.hartono@outlook.com>
2021-10-27 17:32:17 +0700

    remove library load, change readxl to rio, rename column names

--- R/source_thailand.R
+++ R/source_thailand.R
@@ -1,5 +1,3 @@
-library(readxl)
-
 thailand.iso2s <- function(){
   'TH'
 }
@@ -7,7 +5,6 @@ thailand.iso2s <- function(){
 #' Collect generation data from Thailand's Energy Policy and Planning Office (EPPO)
 #'
 #' @param date_from 
-#' @param date_to 
 #'
 #' @return
 #' @export
@@ -16,17 +13,17 @@ thailand.iso2s <- function(){
 
 thailand.collect_generation <- function(date_from){
   url <- 'http://www.eppo.go.th/epposite/images/Energy-Statistics/energyinformation/Energy_Statistics/Electricity/T05_02_02-1.xls'
-  download.file(url, 'thailand.xls', mode='wb')
-  gen <- read_excel('thailand.xls', skip=5)
+  # download.file(url, 'thailand.xls', mode='wb')
+  gen <- rio::import(file = url)
   months <- c('JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC')
-  names(gen) <- c('Date','Hydro','Fuel Oil','Coal','Gas','Diesel','Imported','Renewables','Total')
-  gen %>% select(!Total) %>%
-    filter(Date %in% months) %>% 
-    mutate(Year = rep(1986:(year(today())-1), each=12)) %>%
-    pivot_longer(cols = c(-Date, -Year), names_to='Source', values_to='Output_GWh') %>%
-    mutate(iso2='TH', region='Thailand', data_source='thailand', Date=paste(Date, Year), 
-           across(Output_GWh, as.numeric)) %>%
-  select(-Year) %>%
-  filter(lubridate::my(Date) >= lubridate::ymd(date_from))
+  names(gen) <- c('date','hydro','fuel oil','coal','gas','diesel','imported','renewables','total')
+  gen %>% select(!total) %>%
+    filter(date %in% months) %>% 
+    mutate(year = rep(1986:(year(today())-1), each=12)) %>%
+    pivot_longer(cols = c(-date, -year), names_to='source', values_to='output_gwh') %>%
+    mutate(iso2='TH', region='Thailand', data_source='thailand', date=paste(date, year), 
+           across(output_gwh, as.numeric)) %>%
+  select(-year) %>%
+  filter(lubridate::my(date) >= lubridate::ymd(date_from))
 }
 

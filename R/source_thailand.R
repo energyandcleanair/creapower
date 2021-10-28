9cfc01490ef69064619b624ec553bdbde8780e1e
Hubert Thieriot <hubert@energyandcleanair.org>
2021-10-28 11:19:51 +0800

    -make thailand comparable to other sources

--- R/source_thailand.R
+++ R/source_thailand.R
@@ -11,19 +11,26 @@ thailand.iso2s <- function(){
 #'
 #' @examples
 
-thailand.collect_generation <- function(date_from){
+thailand.collect_generation <- function(date_from, date_to=lubridate::today(tzone="UTC")+1, iso2s=NULL){
   url <- 'http://www.eppo.go.th/epposite/images/Energy-Statistics/energyinformation/Energy_Statistics/Electricity/T05_02_02-1.xls'
   # download.file(url, 'thailand.xls', mode='wb')
   gen <- rio::import(file = url)
+  
   months <- c('JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC')
-  names(gen) <- c('date','hydro','fuel oil','coal','gas','diesel','imported','renewables','total')
-  gen %>% select(!total) %>%
+  names(gen) <- c('date','Hydro','Fuel oil','Coal','Fossil Gas','Diesel','Imported','Renewables','Total')
+  
+  gen %>%
     filter(date %in% months) %>% 
-    mutate(year = rep(1986:(year(today())-1), each=12)) %>%
+    mutate(year = rep(1986:(lubridate::year(lubridate::today())-1), each=12)) %>%
     pivot_longer(cols = c(-date, -year), names_to='source', values_to='output_gwh') %>%
     mutate(iso2='TH', region='Thailand', data_source='thailand', date=paste(date, year), 
-           across(output_gwh, as.numeric)) %>%
-  select(-year) %>%
-  filter(lubridate::my(date) >= lubridate::ymd(date_from))
+           across(output_gwh, as.numeric),
+           date=strptime(paste("01",date), "%d %b %Y"),
+           duration_hours=lubridate::days_in_month(date)*24,
+           output_mw=output_gwh*1000/duration_hours) %>%
+    filter(date >= date_from,
+         date <= date_to,
+         !source %in% c("Total", "Imported")) %>%
+    select(iso2, region, data_source, date, source, output_mw, duration_hours)
 }
 

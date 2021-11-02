ca98b4426e3ba7590bcc246674023e8692eb5c04
Hubert Thieriot <hubert@energyandcleanair.org>
2021-11-02 11:07:07 +0800

    -adding current year data for Thailand

--- R/source_thailand.R
+++ R/source_thailand.R
@@ -12,18 +12,37 @@ thailand.iso2s <- function(){
 #' @examples
 
 thailand.collect_generation <- function(date_from, date_to=lubridate::today(tzone="UTC")+1, iso2s=NULL){
-  url <- 'http://www.eppo.go.th/epposite/images/Energy-Statistics/energyinformation/Energy_Statistics/Electricity/T05_02_02-1.xls'
-  # download.file(url, 'thailand.xls', mode='wb')
-  gen <- rio::import(file = url)
+  
+  # Historical
+  url_hist <- 'http://www.eppo.go.th/epposite/images/Energy-Statistics/energyinformation/Energy_Statistics/Electricity/T05_02_02-1.xls'
+  
+  # Current year
+  url_current <- 'http://www.eppo.go.th/epposite/images/Energy-Statistics/energyinformation/Energy_Statistics/Electricity/T05_02_02.xls'
+  
+  gen_hist <- rio::import(file = url_hist)
+  gen_current <- rio::import(file = url_current)
   
   months <- c('JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC')
-  names(gen) <- c('date','Hydro','Fuel oil','Coal','Fossil Gas','Diesel','Imported','Renewables','Total')
+  names(gen_hist) <- c('date','Hydro','Fuel oil','Coal','Fossil Gas','Diesel','Imported','Renewables','Total')
+  names(gen_current) <- c('year', 'date','Hydro','Fuel oil','Coal','Fossil Gas','Diesel','Imported','Renewables','Total')
   
-  gen %>%
-    filter(date %in% months) %>% 
-    mutate(year = rep(1986:(lubridate::year(lubridate::today())-1), each=12)) %>%
+  
+  bind_rows(
+    tibble(gen_current) %>%
+      tidyr::fill(year, .direction="down") %>%
+      filter(date %in% months) %>%
+      filter(Total>1000), # Exclude the fuel share(%) table
+    
+    tibble(gen_hist) %>%
+      mutate(year=ifelse(grepl("19|20", date) & is.na(Total), date, NA)) %>%
+      tidyr::fill(year, .direction="down") %>%
+      filter(date %in% months)
+    ) %>%
     pivot_longer(cols = c(-date, -year), names_to='source', values_to='output_gwh') %>%
-    mutate(iso2='TH', region='Thailand', data_source='thailand', date=paste(date, year), 
+    distinct(year, date, source, .keep_all=T) %>%
+    mutate(output_gwh=tidyr::replace_na(output_gwh, 0)) %>%
+    mutate(iso2='TH', region='Thailand', data_source='thailand',
+           date=paste(date, year), 
            across(output_gwh, as.numeric),
            date=strptime(paste("01",date), "%d %b %Y"),
            duration_hours=lubridate::days_in_month(date)*24,

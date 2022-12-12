6c1dbd99e55eb89632c77056c613babe103c6de3
Danny Hartono <danny.hartono@outlook.com>
2022-12-12 13:58:21 +0700

    fix iso2s

--- inst/shiny/global.R
+++ inst/shiny/global.R
@@ -39,31 +39,16 @@ preset_params <- list(
   )
 )
 
-# Download cache for latest year
-# lapply(data_sources, function(ds) creapower::data.download_cache(ds, lubridate::year(lubridate::today()), freq="day"))
-
-# TODO remove this function, move to creapower 
-get_generation <- function(date_from,
-                           date_to=lubridate::today() + 1,
-                           # data_source=available_data_sources(),
-                           country=NULL,
-                           freq='daily'){
-  api_url <- 'https://api.energyandcleanair.org/power/generation' # ?date_from={date_from}&date_to={date_to}&format=csv'
-  api_data <- httr::content(httr::GET(api_url, query = list(date_from = date_from, date_to = date_to, 
-                                                            format = 'csv', country = country,
-                                                            frequency = freq)))
-  
-  return(api_data)
-}
-
 # get data from api for last 5 years
 current_year <- lubridate::year(lubridate::today())
-api_data <- get_generation(date_from = sprintf("%s-01-01", current_year - 4)) # TODO change to creapower 
+api_data <- get_generation_api(date_from = sprintf("%s-01-01", current_year - 4)) %>%
+  filter(!is.na(country)) # database has empty country and/or region
 
 data_sources <- available_data_sources()
 iso2s <- api_data %>% distinct(country) %>%
-  mutate(iso2 = countrycode::countrycode(avail_countries, origin = 'country.name', 
+  mutate(iso2 = countrycode::countrycode(country, origin = 'country.name', 
                                          destination = 'iso2c', custom_match = c("Kosovo"="XK")))
+
 sources <- names(palette_power())
 countries <- c(iso2s$iso2) %>% `names<-`(iso2s$country)
 countries['European Union'] <- 'EU'

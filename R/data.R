cd5a9465d0fe67e9e3bc388925ab87801daa48c7
Danny Hartono <danny.hartono@outlook.com>
2022-12-12 12:18:43 +0700

    re-add old functions

--- R/data.R
+++ R/data.R
@@ -25,38 +25,34 @@ data_source_reference <- function(data_source){
 }
 
 
-# available_iso2s <- function(){
-#   lapply(available_data_sources(), function(ds){
-#     get(sprintf("%s.iso2s",ds))() %>%
-#       tibble(iso2=.) %>%
-#       mutate(data_source=ds,
-#              region=countrycode::countrycode(iso2, "iso2c", "country.name",
-#                                              custom_match =c("EU"="European Union")))
-#   }) %>%
-#     do.call(bind_rows, .)
-# }
+available_iso2s <- function(){
+  lapply(available_data_sources(), function(ds){
+    get(sprintf("%s.iso2s",ds))() %>%
+      tibble(iso2=.) %>%
+      mutate(data_source=ds,
+             region=countrycode::countrycode(iso2, "iso2c", "country.name",
+                                             custom_match =c("EU"="European Union")))
+  }) %>%
+    do.call(bind_rows, .)
+}
 
 
-# data.download <- function(){
-#   
-# }
-
-# data.source_homogenising_greps <- function(){
-#   list(
-#     "Thermal"= "^Thermal$",
-#     "Coal"= "Coal|Lignite",
-#     "Fossil Gas"= "Fossil Gas|Natural Gas",
-#     # "Oil"= "Oil",
-#     "Nuclear"= "Nuclear",
-#     "Hydro"= "Hydro",
-#     "Wind"= "Wind",
-#     "Solar"= "Solar",
-#     "Renewables" = "^Renewables$|^Renewable$",
-#     # "Other Renewables"= "Geothermal|Renewable|Marine|Biomass",
-#     # "Other" = "Peat|^Other$|Waste"
-#     "Other"= "Geothermal|Other Renewable|Marine|Biomass|Peat|^Other$|Waste|Oil|Diesel"
-#   )
-# }
+data.source_homogenising_greps <- function(){
+  list(
+    "Thermal"= "^Thermal$",
+    "Coal"= "Coal|Lignite",
+    "Fossil Gas"= "Fossil Gas|Natural Gas",
+    # "Oil"= "Oil",
+    "Nuclear"= "Nuclear",
+    "Hydro"= "Hydro",
+    "Wind"= "Wind",
+    "Solar"= "Solar",
+    "Renewables" = "^Renewables$|^Renewable$",
+    # "Other Renewables"= "Geothermal|Renewable|Marine|Biomass",
+    # "Other" = "Peat|^Other$|Waste"
+    "Other"= "Geothermal|Other Renewable|Marine|Biomass|Peat|^Other$|Waste|Oil|Diesel"
+  )
+}
 
 #' Download
 #'
@@ -68,21 +64,21 @@ data_source_reference <- function(data_source){
 #' @export
 #'
 #' @examples
-# data.download_cache <- function(data_source, year, force=F, cache_folder="cache", freq=NULL){
-# 
-#   dir.create(file.path(cache_folder, data_source), showWarnings = F, recursive = T)
-# 
-#   if(is.null(freq) || grepl("hour", freq, ignore.case = T)){
-#     file_base <- file.path(data_source, sprintf("gen_%d.RDS", year))
-#     file_cache <- file.path(cache_folder, file_base)
-#   }else{
-#     file_base <- file.path(data_source, sprintf("gen_daily_%d.RDS", year))
-#     file_cache <- file.path(cache_folder, file_base)
-#   }
-# 
-#   message("Checking online cache version: ", file_base)
-#   gcs.download(source_path=file_base, dest_path=file_cache, only_if_modified_since = force)
-# 
-#   return(file_cache)
-# }
+data.download_cache <- function(data_source, year, force=F, cache_folder="cache", freq=NULL){
+
+  dir.create(file.path(cache_folder, data_source), showWarnings = F, recursive = T)
+
+  if(is.null(freq) || grepl("hour", freq, ignore.case = T)){
+    file_base <- file.path(data_source, sprintf("gen_%d.RDS", year))
+    file_cache <- file.path(cache_folder, file_base)
+  }else{
+    file_base <- file.path(data_source, sprintf("gen_daily_%d.RDS", year))
+    file_cache <- file.path(cache_folder, file_base)
+  }
+
+  message("Checking online cache version: ", file_base)
+  gcs.download(source_path=file_base, dest_path=file_cache, only_if_modified_since = force)
+
+  return(file_cache)
+}
 

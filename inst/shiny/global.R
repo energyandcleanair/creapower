6ebb1c794f612020da172931a4b1312d312b2ec4
Danny Hartono <danny.hartono@outlook.com>
2023-01-17 14:21:01 +0700

    - add variables for gas tab - add min_years for all data

--- inst/shiny/global.R
+++ inst/shiny/global.R
@@ -41,17 +41,49 @@ preset_params <- list(
   )
 )
 
-# get data from api for last 5 years
+min_years <- c( # for dynamic date_from
+  "entsoe"=2015,
+  "EIA"=2018,
+  "posoco_in"=2017,
+  "BMRS"=2015,
+  "japan"=2016,
+  "EPPO"=2015,
+  "southkorea_kr"=2016,
+  "southafrica_za"=2018,
+  "TEIAS"=2016,
+  "iemop_ph"=2022,
+  "newmweb_au"=2022,
+  "nldc_vn"=2020,
+  "wind_cn"=2014,
+  "crea"=2022
+)
+
+# for gas tab
+data_types <- c(
+  "Fossil Gas"="fossil_gas",
+  "Fossil Gas (Temperature Corrected)"="fossil_gas_temperature_corrected",
+  "Electricity (Temperature Corrected)"="electricity_temperature_corrected")
+
+# get power data from api for last 4 years
+print("Getting power data")
 current_year <- lubridate::year(lubridate::today())
-downloaded_years <- (current_year - 4):current_year
-api_data <- get_generation_api(date_from = sprintf("%s-01-01", current_year - 4)) %>%
+downloaded_years <- (current_year - 3):current_year
+api_data <- get_generation_api(date_from = sprintf("%s-01-01", current_year - 3), freq = 'daily,monthly') %>%
   filter(!is.na(country)) # database has empty country and/or region
 
 data_sources <- available_data_sources()
 iso2s <- api_data %>% distinct(country) %>%
-  mutate(iso2 = countrycode::countrycode(country, origin = 'country.name', 
+  mutate(iso2 = countrycode::countrycode(country, origin = 'country.name',
                                          destination = 'iso2c', custom_match = c("Kosovo"="XK")))
 
 sources <- names(palette_power())
 countries <- c(iso2s$iso2) %>% `names<-`(iso2s$country)
 countries['European Union'] <- 'EU'
+
+# get gas demand data from api for last 2 years
+gas_api_data <- get_gas_api(date_from = sprintf("%s-01-01", current_year - 1))
+
+downloaded_years_gas <- (current_year - 1):current_year
+iso2s_gas <- gas_api_data %>% distinct(region_id, country) %>%
+  replace_na(list(country = 'European Union'))
+countries_gas <- c(iso2s_gas$region_id) %>% `names<-`(iso2s_gas$country)
